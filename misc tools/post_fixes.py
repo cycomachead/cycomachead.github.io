d4f2387353153db18a89acede0535e417df7a832
Michael Ball <cycomachead@gmail.com>
2012-12-26 16:35:58 -0800

    working on post changes per flickr url ideas

---
+++ misc tools/post_fixes.py
@@ -0,0 +1,47 @@
+"""
+Given a DIR, search for FIND in each file and replace it with REPLACE:
+"""
+
+import sys, fileinput, glob, os, re
+from os.path import join
+
+files_dir = ''
+inp_str = ''
+out_str = ''
+files = []
+
+def setup():
+    global files_dir, rep_str, out_str, files
+    try:
+        files_dir = sys.argv[1]
+        rep_str = sys.argv[2]
+        out_str = open(sys.argv[3], 'r').read()
+        files = os.listdir()
+    except IndexError:
+        print("Usage: FILE FIND REPLACE")
+        exit()
+
+def replace(file, pattern, subst):
+    # Read contents from file as a single string
+    file_handle = open(file, 'r')
+    file_string = file_handle.read()
+    file_handle.close()
+
+    # Use RE package to allow for replacement (also allowing for (multiline) REGEX)
+    file_string = (re.sub(pattern, subst, file_string))
+
+    # Write contents to file.
+    # Using mode 'w' truncates the file.
+    file_handle = open(file, 'w')
+    file_handle.write(file_string)
+    file_handle.close()
+
+if __name__ == '__main__':
+    setup()
+    print(out_str, "\n")
+    print(files)
+    rep_str = "farmid: FARM
+serid: SERVER
+secret: SECRET"
+    for file in files:
+            replace(file, rep_str, out_str)
