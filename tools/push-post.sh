cdcb4184e445218b1af8347445a0ad51a0534835
Michael Ball <cycomachead@gmail.com>
2013-01-01 14:57:25 -0800

    Added new post

--- tools/push-post.sh
+++ tools/push-post.sh
@@ -7,7 +7,7 @@ git add *
 git commit -a -m "Added new post"
 git push
 git checkout gh-pages
-mv -r _site/* .
+mv -f _site/* .
 git commit -a -m "Added new post to complied branch"
 git push
 git checkout source
