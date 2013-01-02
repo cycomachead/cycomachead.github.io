bd27e658baf1589ca7003ded5355112b27548696
Michael Ball <cycomachead@gmail.com>
2013-01-01 16:58:25 -0800

    Added new post

--- tools/push-post.sh
+++ tools/push-post.sh
@@ -1,4 +1,10 @@
 # do the work to actually push a post to gh.
+if [ -z "$1" ]
+    then
+        msg = "Added post to compiled repo"
+    else
+        msg = "$1"
+fi
 
 cd ~/Dropbox/Projects/Website
 jekyll
@@ -8,5 +14,5 @@ git push
 cp -r _site/* ../michaelballphoto.com/
 cd ../michaelballphoto.com
 git add *
-git commit -a -m "Added new post to complied repo"
+git commit -a -m "$msg"
 git push
